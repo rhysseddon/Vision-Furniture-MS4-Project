3d7ffd7bf9156f8f10cc560e1d1d2611392d36a2
rhysseddon <rhysseddon@hotmail.com>
2021-03-24 10:57:54 +0000

    Add add to cart functionality

--- cart/contexts.py
+++ cart/contexts.py
@@ -9,13 +9,11 @@ def cart_contents(request):
     cart_items = []
     product_count = 0
     sub_total = 0
-    grand_total = sub_total + Product.delivery_cost
 
     context = {
         'cart_items': cart_items,
         'sub_total': sub_total,
         'product_count': product_count,
-        'grand_total': grand_total,
     }
 
     return context
