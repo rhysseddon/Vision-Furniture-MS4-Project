57d40b7e4bbd8eb3679e615f294a8c03740c399e
rhysseddon <rhysseddon@hotmail.com>
2021-03-24 12:13:38 +0000

    Update context processor and shopping bag template

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 
 from products.models import Product
 
@@ -9,11 +10,27 @@ def cart_contents(request):
     cart_items = []
     product_count = 0
     sub_total = 0
+    delivery = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        sub_total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    grand_total = delivery + sub_total
 
     context = {
         'cart_items': cart_items,
         'sub_total': sub_total,
         'product_count': product_count,
+        'delivery': delivery,
+        'grand_total': grand_total,
     }
 
     return context
