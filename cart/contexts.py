9ea3a4f19cc063402dbff270ad02f2e9671f68db
rhysseddon <rhysseddon@hotmail.com>
2021-03-25 11:28:16 +0000

    Add toasts

--- cart/contexts.py
+++ cart/contexts.py
@@ -10,19 +10,21 @@ def cart_contents(request):
     cart_items = []
     product_count = 0
     sub_total = 0
+    delivery = 0
     cart = request.session.get('cart', {})
 
     for item_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=item_id)
+        delivery = product.delivery_cost
         sub_total += quantity * product.price
         product_count += quantity
         cart_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
+            'delivery': delivery,
         })
 
-    delivery = product.delivery_cost
     grand_total = delivery + sub_total
 
     context = {
