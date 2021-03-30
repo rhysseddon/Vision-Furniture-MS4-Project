bf3a590599a01002f69f0b924b45b3da086a10cd
rhysseddon <rhysseddon@hotmail.com>
2021-03-30 13:00:23 +0000

    Update delivery costs

--- cart/contexts.py
+++ cart/contexts.py
@@ -15,8 +15,17 @@ def cart_contents(request):
 
     for item_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=item_id)
-        delivery = product.delivery_cost
         sub_total += quantity * product.price
+        if sub_total >= 400:
+            delivery = 40
+        elif sub_total >= 150:
+            delivery = 25
+        elif sub_total >= 50:
+            delivery = 10
+        elif sub_total >= 20:
+            delivery = 7
+        else:
+            delivery = 5
         product_count += quantity
         cart_items.append({
             'item_id': item_id,
