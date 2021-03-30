6a70d4b74c71272098c0e0bd3950297d10e1ae40
rhysseddon <rhysseddon@hotmail.com>
2021-03-30 17:25:23 +0000

    Edit update/remove product buttons

--- favorites/views.py
+++ favorites/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_favorites(request):
     """ A view that will render the favorites page """
 
     return render(request, 'favorites/favorites.html')
+
+
+# def add_to_favorites(request, item_id):
+#     """ Add products to the favorites page """
+#     redirect_url = request.POST.get('redirect_url')
+#     favorites = request.session.get('favorites', {})
+#     print(request.session['favorites'])
+
+#     return redirect(redirect_url)
