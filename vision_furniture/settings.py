1fbf039bb0a72ee4895efaf7306433f7337ac9f3
rhysseddon <rhysseddon@hotmail.com>
2021-04-08 11:31:42 +0000

    Remove secret key and set debug

--- vision_furniture/settings.py
+++ vision_furniture/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-+za_5@ubpas)8-9h+1hgp#_net%(+c(zg5s6np&o57842swex'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['vision-furniture.herokuapp.com', 'localhost']
 
