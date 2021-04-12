a07fa861b61e1d10a73bbc389b7aa8d60c82dd1d
rhysseddon <rhysseddon@hotmail.com>
2021-04-12 16:55:57 +0000

    Add Help model to home app

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Help(models.Model):
+    """
+    A model for admin to edit and add FAQ
+    """
+    class Meta:
+        verbose_name_plural = 'Help'
+
+    question = models.CharField(max_length=254)
+    answer = models.TextField()
+
+    def __str__(self):
+        return self.question
