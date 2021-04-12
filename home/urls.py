19701c7c217e28314d14b6ea8313da1f4f0bbf38
rhysseddon <rhysseddon@hotmail.com>
2021-04-12 17:21:08 +0000

    Add help_centre url, view and help_centre.html and linked to help button in footer

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('help/', views.help_centre, name='help_centre')
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Help
 
 
 def index(request):
     """ A view that will return the index page """
 
     return render(request, 'home/index.html')
+
+
+def help_centre(request):
+    """ A view that will render the help centre page """
+
+    return render(request, 'home/help_centre.html')
